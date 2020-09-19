be427856e4c02fe79e4ee7836fbaa8dd5f354a9b
ArnobMahmud <arnob.tech.me@gmail.com>
2020-09-19 22:22:18 +0600

    Effects Added

--- lib/pages/developer_page.dart
+++ lib/pages/developer_page.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -7,18 +6,37 @@ class DevelopersPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFfff8e3),
       body: ListView(
         children: [
           Container(
+            //  margin: EdgeInsets.all(5.0),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(40),
+                  bottomRight: Radius.circular(40),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 10,
+                      offset: Offset(0, 7))
+                ]),
             child: Stack(
               children: [
-                Image.asset(
-                  "images/linux.jpg",
-                  fit: BoxFit.cover,
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(40),
+                      bottomRight: Radius.circular(40)),
+                  child: Image.asset(
+                    "images/linux.jpg",
+                    fit: BoxFit.cover,
+                  ),
                 ),
                 Positioned(
-                  left: 25,
-                  top: 160,
+                  left: 35,
+                  top: 175,
                   child: CircleAvatar(
                     radius: 40,
                     backgroundImage: AssetImage("images/logo.jpg"),
@@ -28,29 +46,53 @@ class DevelopersPage extends StatelessWidget {
             ),
           ),
           SizedBox(
-            height: 20,
+            height: 10,
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
+              decoration: BoxDecoration(
+                  color: Color(0xFFf7abb9),
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 10,
+                      offset: Offset(0, 7),
+                    )
+                  ]),
+              margin: EdgeInsets.only(right: 70, left: 70),
+              height: 60,
               alignment: Alignment.center,
-              child: Text(
-                "Developers",
-                style: TextStyle(
-                  color: Color(0xFF601852),
-                  fontFamily: "Ubuntu",
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(
+                    Icons.settings,
+                    color: Color(0xFF1c223d),
+                    size: 32,
+                  ),
+                  Text(
+                    "Developers",
+                    style: TextStyle(
+                      color: Color(0xFF4d1d35),
+                      fontFamily: "Ubuntu",
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
-          SizedBox(height: 10,),
+          SizedBox(
+            height: 5.0,
+          ),
           Container(
             height: 100,
             child: Padding(
               padding: const EdgeInsets.all(8.0),
-
               child: ListTile(
                 leading: CircleAvatar(
                   radius: 30,
@@ -65,20 +107,26 @@ class DevelopersPage extends StatelessWidget {
                       color: Color(0xFF393949)),
                 ),
                 subtitle: Text(
-                    "BSc in Textile Engineering, Bangladesh University of Textiles (BUTEX)",
-                    style: TextStyle(
-                        color: Color(0xFF440544),
-                        fontFamily: "Ubuntu",
-                        fontSize: 16),
+                  "BSc in Textile Engineering, Bangladesh University of Textiles (BUTEX)",
+                  style: TextStyle(
+                    color: Color(0xFF440544),
+                    fontFamily: "Ubuntu",
+                    fontSize: 17,
                   ),
-                  trailing: IconButton(
-                    icon: Icon(Icons.message_outlined), onPressed: () {}),
                 ),
-                
+                trailing: IconButton(
+                    icon: Icon(
+                      Icons.message_outlined,
+                      color: Color(0xFF504682),
+                    ),
+                    onPressed: () {}),
               ),
             ),
-            SizedBox(height: 20,),
-            Container(
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          Container(
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: ListTile(
@@ -95,34 +143,38 @@ class DevelopersPage extends StatelessWidget {
                       color: Color(0xFF393949)),
                 ),
                 subtitle: Text(
-                    "BSc in Computer Science and Engineering, North South University (NSU)",
-                    style: TextStyle(
-                        color: Color(0xFF440544),
-                        fontFamily: "Ubuntu",
-                        fontSize: 16),
-                  ),
-                  trailing: IconButton(
-                    icon: Icon(Icons.message_outlined), onPressed: () {
-                      
-                    }),
+                  "BSc in Computer Science and Engineering, North South University (NSU)",
+                  style: TextStyle(
+                      color: Color(0xFF440544),
+                      fontFamily: "Ubuntu",
+                      fontSize: 17),
                 ),
-                
+                trailing: IconButton(
+                    icon: Icon(
+                      Icons.message_outlined,
+                      color: Color(0xFF504682),
+                    ),
+                    onPressed: () {}),
               ),
             ),
-            SizedBox(height: 20,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("We Are The Geeks of RippleBee ❤️",
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "We Are The Geeks of RippleBee ❤️",
                 style: TextStyle(
-                  color: Color(0xFF122d70),
+                  color: Color(0xFF785978),
                   fontSize: 16,
                   fontFamily: "Ubuntu",
                   fontWeight: FontWeight.w700,
-
-                ),)
-              ],
-            )
+                ),
+              )
+            ],
+          )
         ],
       ),
     );
